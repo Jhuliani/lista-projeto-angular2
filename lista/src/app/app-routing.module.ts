4cff66ca6ffd1107c31adb600ea60ef51add8125
Viviane Perrotti <viperrotti@gmail.com>
2023-01-19 10:43:59 -0300

    formatacao lista

--- lista/src/app/app-routing.module.ts
+++ lista/src/app/app-routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
     component: ExcluirItemComponent
   },
   {
-    path: 'itens/editaritem/:id',
+    path: 'itens/editarItem/:id',
     component: EditarItemComponent
 }
 ];
